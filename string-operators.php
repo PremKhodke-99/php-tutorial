aafbb3fd1251d9af35cf131e43821902e8bdf696
Prem <premkhodke.99@gmail.com>
2024-10-06 21:19:35 +0530

    concatenation

---
+++ string-operators.php
@@ -0,0 +1,13 @@
+<?php 
+$name = "prem khodke";
+$str1 = "peter";
+$str = "10";
+$str2 = "my name is ";
+
+// echo "my name is ".$name;
+// echo $str2 ." ".$name;
+
+$str2.=$str1; //concatenation 
+
+echo $str2;
+?>
