35ff54a56c11eb4c7486fb570dbfc85e3ec199db
Prem <premkhodke.99@gmail.com>
2024-10-13 15:43:44 +0530

    fn of php

---
+++ 10-important-function.php
@@ -0,0 +1,35 @@
+<?php 
+// $str = "prem khodke";
+// echo strlen( $str);
+
+// $var = 10;
+// var_dump( $var );
+
+// echo date('d');
+// echo date('l');
+// echo date('m');
+
+// echo phpinfo() ;
+
+// $var = "abc";
+// echo is_string($var);
+
+// $var = 123;
+// echo is_int($var);
+
+// echo rand();
+
+// echo "before";
+// die();
+// exit();
+
+// echo "after";
+
+
+// $str = "codestepbystep";
+// echo substr( $str,5) . "<br/>";
+// echo substr( $str,4, 5) . "<br />";
+// echo substr( $str, -6) . "<br />";
+
+
+?>
