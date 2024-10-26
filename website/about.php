14bc7d7da1fed8379cd8a77466f62b1f595cdddb
Prem <premkhodke.99@gmail.com>
2024-10-26 21:37:37 +0530

    website created with php html

---
+++ website/about.php
@@ -0,0 +1,7 @@
+<?php
+include_once("header.php");
+?>
+<h3>About hu</h3>
+<?php
+include_once("footer.php");
+?>
---
+++ website/header.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <h3>
+        Logo
+    </h3>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</div>
